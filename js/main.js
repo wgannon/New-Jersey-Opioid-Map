47744821164069de1c0b4fc2dceecf60a7107759
wgannon <42075666+wgannon@users.noreply.github.com>
2019-04-09 00:06:15 -0400

    Fixed the Alginment issue
    Changed my svg and made edits to the text to make easier to read.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,13 @@
 				];
 	var expressed = attrArray[0];
 
+	var chartWidth = window.innerWidth * 0.425,
+		chartHeight = 460;
+	//create a scale to size bars proportionally to frame
+	var yScale = d3.scale.linear()
+		.range([0, chartHeight])
+		.domain([0, 600]);	
+	
 	window.onload = setMap(); //start script once HTML is loaded
 
 
@@ -65,12 +72,47 @@
 
 			//add enumeration units to the map
 			setEnumerationUnits(counties, map, path, colorScale);
+			
 			//add coordinated visualizations to map
 			setChart(csvData, colorScale);
 
 		}; //end of callback
 		
 	}; //end of setmap
+		//function to create color scale generator
+	function makeColorScale(data) {
+		var colorClasses = [
+        "#fef0d9",
+        "#fdcc8a",
+        "#fc8d59",
+        "#e34a33",
+        "#b30000"
+    ];
+
+		//create color scale generator
+		var colorScale = d3.scale.threshold()
+			.range(colorClasses);
+
+		//build array of all values of the expressed attribute
+		var domainArray = [];
+		for (var i = 0; i < data.length; i++) {
+			var val = parseFloat(data[i][expressed]);
+			domainArray.push(val);
+		};
+
+		//cluster data using ckmeans clustering algorithm to create natural breaks
+		var clusters = ss.ckmeans(domainArray, 5);
+		//reset domain array to cluster minimums
+		domainArray = clusters.map(function (d) {
+			return d3.min(d);
+		});
+		//remove first value from domain array to create class breakpoints
+		domainArray.shift();
+
+		//assign array of last 4 cluster minimums as domain
+		colorScale.domain(domainArray);
+		return colorScale;
+	};//End of make c
 	function choropleth(props, colorScale) {
 		//make sure attribute value is a number
 		var val = parseFloat(props[expressed]);
@@ -81,10 +123,62 @@
 			return "#CCC";
 		};
 	};
+	function setGraticule(map, path) {
+		var graticule = d3.geo.graticule()
+			.step([1.5, 1.5]); //place graticule lines every 10 degrees of longitude and latitude
+
+
+		//create graticule lines	
+		var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+			.data(graticule.lines) //bind graticule lines to each element to be created
+			.enter() //create an element for each datum
+			.append("path") //append each element to the svg as a path element
+			.attr("class", "gratLines") //assign class for styling
+			.attr("d", path); //project graticule lines	
+
+	}; //end of setGraticule
+	function joinData(counties, csvData) {
+		for (var i = 0; i < csvData.length; i++) {
+			var csvCounties = csvData[i];
+			var csvKey = csvCounties.OBJECTID; //The CSV Pripary Key
+			//Loop through geojson counties to find correct county
+			for (var a = 0; a < counties.length; a++) {
+
+				var geojsonProps = counties[a].properties; //the current county geojson properties
+				var geojsonKey = geojsonProps.OBJECTID; //the geojson primary key
+
+				//where primary keys match, transfer csv data to geojson properties object
+				if (geojsonKey == csvKey) {
+
+					//assign all attributes and values
+					attrArray.forEach(function (attr) {
+						var val = parseFloat(csvCounties[attr]); //get csv attribute value
+						geojsonProps[attr] = val; //assign attribute and value to geojson properties
+					});
+				};
+			};
+		}; //end of for loop
+		return counties;
+
+	}; //end of join data
+	function setEnumerationUnits(counties, map, path, colorScale) {
+		//add NJ Counties to map
+		var njcounties = map.selectAll(".njcounties")
+			.data(counties)
+			.enter()
+			.append("path")
+			.attr("class", function (d) {
+				return "njcounties " + d.properties.OBJECTID;
+			})
+			.attr("d", path)
+			.style("fill", function (d) {
+				return choropleth(d.properties, colorScale);
+			});
+	}; //end of setEnumerationUnits
+	
 	function setChart(csvData, colorScale) {
 		//chart frame dimensions
-		var chartWidth = window.innerWidth * 0.425,
-			chartHeight = 460;
+
 		//Example 2.1 line 17...create a second svg element to hold the bar chart
 		var chart = d3.select("body")
 			.append("svg")
@@ -92,11 +186,7 @@
 			.attr("width", chartWidth)
 			.attr("height", chartHeight);
         
-        
-		//create a scale to size bars proportionally to frame
-		var yScale = d3.scale.linear()
-			.range([0, chartHeight])
-			.domain([0, 600]);
+
 		//set bars for each province
 		var bars = chart.selectAll(".bars")
 			.data(csvData)
@@ -138,109 +228,22 @@
 				return i * fraction + (fraction - 1) / 2;
 			})
 			.attr("y", function (d) {
-				return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+				return chartHeight - yScale(parseFloat(d[expressed])) +17 ;
 			})
 			.text(function (d) {
 				return d[expressed];
 			});		
-		//create a second svg element to hold the bar chart
-		console.log(chart)
 		//below Example 2.8...create a text element for the chart title
 		var chartTitle = chart.append("text")
 			.attr("x", 20)
 			.attr("y", 40)
 			.attr("class", "chartTitle")
 			.text("Count of " + expressed+ "per 100k");
-		console.log(expressed)
 	}; //end of set chart
 	
-	function setGraticule(map, path) {
-		var graticule = d3.geo.graticule()
-			.step([1.5, 1.5]); //place graticule lines every 10 degrees of longitude and latitude
 
 
-		//create graticule lines	
-		var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-			.data(graticule.lines) //bind graticule lines to each element to be created
-			.enter() //create an element for each datum
-			.append("path") //append each element to the svg as a path element
-			.attr("class", "gratLines") //assign class for styling
-			.attr("d", path); //project graticule lines	
 
-	}; //end of setGraticule
-	function joinData(counties, csvData) {
-		for (var i = 0; i < csvData.length; i++) {
-			var csvCounties = csvData[i];
-			var csvKey = csvCounties.OBJECTID; //The CSV Pripary Key
-			//Loop through geojson counties to find correct county
-			for (var a = 0; a < counties.length; a++) {
-
-				var geojsonProps = counties[a].properties; //the current county geojson properties
-				var geojsonKey = geojsonProps.OBJECTID; //the geojson primary key
-
-				//where primary keys match, transfer csv data to geojson properties object
-				if (geojsonKey == csvKey) {
-
-					//assign all attributes and values
-					attrArray.forEach(function (attr) {
-						var val = parseFloat(csvCounties[attr]); //get csv attribute value
-						geojsonProps[attr] = val; //assign attribute and value to geojson properties
-					});
-				};
-			};
-		}; //end of for loop
-		console.log(counties);
-		return counties;
-
-	}; //end of join data
-	function setEnumerationUnits(counties, map, path, colorScale) {
-		//add NJ Counties to map
-		var njcounties = map.selectAll(".njcounties")
-			.data(counties)
-			.enter()
-			.append("path")
-			.attr("class", function (d) {
-				return "njcounties " + d.properties.OBJECTID;
-			})
-			.attr("d", path)
-			.style("fill", function (d) {
-				return choropleth(d.properties, colorScale);
-			});
-	}; //end of setEnumerationUnits
 	
-	//function to create color scale generator
-	function makeColorScale(data) {
-		var colorClasses = [
-        "#D4B9DA",
-        "#C994C7",
-        "#DF65B0",
-        "#DD1C77",
-        "#980043"
-    ];
 
-		//create color scale generator
-		var colorScale = d3.scale.threshold()
-			.range(colorClasses);
-
-		//build array of all values of the expressed attribute
-		var domainArray = [];
-		for (var i = 0; i < data.length; i++) {
-			var val = parseFloat(data[i][expressed]);
-			domainArray.push(val);
-		};
-
-		//cluster data using ckmeans clustering algorithm to create natural breaks
-		var clusters = ss.ckmeans(domainArray, 5);
-		//reset domain array to cluster minimums
-		domainArray = clusters.map(function (d) {
-			return d3.min(d);
-		});
-		//remove first value from domain array to create class breakpoints
-		domainArray.shift();
-
-		//assign array of last 4 cluster minimums as domain
-		colorScale.domain(domainArray);
-		console.log(clusters)
-		return colorScale;
-	};//End of make c
 })();
